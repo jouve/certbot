bb130da42743b071ef69fdda1b84d5ebebee0c15
Cyril Jouve <jv.cyril@gmail.com>
2023-05-18 23:46:17 +0200

    use --non-interactive in container

--- certonly.sh
+++ certonly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker () { sudo nerdctl "$@"; }
-docker run -v $PWD/letsencrypt:/etc/letsencrypt jouve/certbot certonly "$@"
+docker run -v $PWD/letsencrypt:/etc/letsencrypt jouve/certbot certonly --non-interactive "$@"
